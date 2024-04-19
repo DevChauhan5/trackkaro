f13f46f57258affff6fad795eddcc4dbd30d055c
Dev Chauhan <124146292+DevChauhan5@users.noreply.github.com>
2024-04-19 12:37:43 +0000

    added expense table schema

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -1,10 +1,23 @@
-import { text, pgTable, timestamp, uuid } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import { text, pgTable, timestamp, uuid, numeric } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
-	id: uuid("id").primaryKey().defaultRandom(),
-	username: text("username").notNull().unique(),
-	email: text("email").notNull().unique(),
-	password: text("password").notNull(),
-	createdAt: timestamp("created_at").notNull().defaultNow(),
-	updatedAt: timestamp("updated_at").notNull().defaultNow(),
+  id: uuid("id").primaryKey().defaultRandom(),
+  username: text("username").notNull().unique(),
+  email: text("email").notNull().unique(),
+  password: text("password").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
+});
+
+export const expenses = pgTable("expenses", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  userId: uuid("user_id")
+    .references(() => users.id)
+    .notNull(),
+  description: text("description").notNull(),
+  amount: numeric("amount").notNull(),
+  category: text("category").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
 });
